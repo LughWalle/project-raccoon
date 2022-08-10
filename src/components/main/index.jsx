6488070ad34074741ca46c8e6f7b65b8ab4e3679
LughWalle <civalfran1@gmail.com>
2022-08-10 20:22:02 -0300

    fix bugs in main

--- src/components/main/index.jsx
+++ src/components/main/index.jsx
@@ -9,11 +9,12 @@ function Main() {
   } = useContext(ProjectContext);
   return (
     <main className={styles.main} id='main'>
-      <div className={styles.bg}></div>
+      <div className={styles.bg}>
       <div className={styles.text}>
         <h1 className={styles.title}>Lorem ipsum</h1>
         <p className={styles.subtitle}>Lorem ipsum dolor sit amet consectetur adipisicing elit</p>
         <Button href="https://github.com/LughWalle" target='_blank' variant='#00A4EF' className={styles.mainBtn} />
+      </div>
       </div>
     </main>
   );
